fd602f5ff5b2e41ec205d8cea828db65fac1968d
Itai Schvartzbard <itai@practitest.com>
2022-05-13 23:14:44 +0300

    styling with styled components

--- src/components/ChatsListScreen/index.tsx
+++ src/components/ChatsListScreen/index.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import ChatsList from "./ChatsList";
 import ChatsNavbar from "./ChatsNavbar";
+import styled from "styled-components";
 
+
+// Styles
+
+const Container = styled.div`
+    height: 100vh;
+`;
 const ChatListScreen: React.FC = () => (
-    <>
+    <Container>
         <ChatsNavbar />
         <ChatsList />
-    </>
+    </Container>
 )
 
 export default ChatListScreen;
